0cc51e1a90d1db1ff9e0142ddadd6f138ac78518
yaocs <yao.cs@qq.com>
2016-09-05 20:22:29 +0800

    update-0905-20:22

--- js/index.js
+++ js/index.js
@@ -72,8 +72,54 @@ $(document).ready(function () {
   // 相册页面
   $('.photoList li').hover(function () {
     $(this).find('span').stop().fadeIn(300);
-  }, function () {;
+  }, function () {
+    ;
     $(this).find('span').stop().fadeOut(300);
+  })
+
+  // =================
+  // 弹出层
+  // =================
+
+  // 弹出
+  $('.viewPhotos').each(function () {
+    $(this).on('click', function () {
+      $('.detailCover').animate({ left: 0 }, 500);
+      $('body').css('overflow-y', 'hidden');
+    })
+  })
+
+  // 关闭按钮
+  $('#closeBtn').on('click', function () {
+    $(this).parents('.detailCover').animate({ left: '100%' }, 500);
+    $('body').css('overflow-y', 'auto');
+  })
+
+  // 轮播
+  var dtIndex = 0;
+  var dtImgSize = $('.picSlideBig li').size();
+  $('.picSlideBig li').eq(0).show();
+  $('.totalNum').text(dtImgSize);
+  $('.curNum').text('1');
+
+  $('#picSlideBtnNext').on('click', function () {
+    dtIndex += 1;
+    if (dtIndex == dtImgSize) {
+      dtIndex = 0;
+      $('.curNum').text('1');
+    }
+    $('.curNum').text(dtIndex + 1);
+    $('.picSlideBig li').eq(dtIndex).fadeIn().siblings().fadeOut();
+  })
+
+  $('#picSlideBtnPrev').on('click', function () {
+    dtIndex -= 1;
+    if (dtIndex < 0) {
+      dtIndex = dtImgSize - 1;
+      $('.curNum').text('8');
+    }
+    $('.curNum').text(dtIndex);
+    $('.picSlideBig li').eq(dtIndex).fadeIn().siblings().fadeOut();
   })
 
 })
