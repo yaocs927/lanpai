71022e8107e087b2c046bebe2052b94eb76e4716
yaocs <yao.cs@qq.com>
2016-09-08 11:28:17 +0800

    update-0908-11:30

--- admin/js/admin.js
+++ admin/js/admin.js
@@ -12,10 +12,14 @@ $(function () {
   })
 
 
+  // =================================
+  // 发布品项
+  // =================================
+
   var num = 1;
   // 品项发布-详情新增一条信息
   $('#fbpx-info-add').on('click', function () {
-    $('#fbpx-info-list tbody').append('<tr><td class="col-md-1"><input type="checkbox" class="checkboxStyle"></td><td class="col-md-2"><input type="text" class="form-control"></td><td class="col-md-6"><input type="text" class="form-control"></td><td class="col-md-1"><button type="button" class="btn btn-warning fbpx-info-delete">删除</button></td></tr>')
+    $('#fbpx-info-list tbody').append('<tr><td class="col-md-1"><input type="checkbox" class="checkboxStyle" name="fbpx-info-list"></td><td class="col-md-2"><input type="text" class="form-control"></td><td class="col-md-6"><input type="text" class="form-control"></td><td class="col-md-1"><button type="button" class="btn btn-warning fbpx-info-delete">删除</button></td></tr>')
   })
 
   // 品项发布-详情删除一条信息
@@ -23,21 +27,34 @@ $(function () {
     $(this).parents('tr').remove();
   })
 
-  // 品项发布-详情新增一条信息
-  $('#fbpx-photo-add').on('click', function () {
-    num ++;
-    $('#fbpx-photo-list tbody').append('<tr><td class="col-md-1"><input type="checkbox" class="checkboxStyle"></td><td class="col-md-6"><input type="text" class="form-control inputfile-'+ num +'"></td><td class="col-md-1"><label class="btn btn-primary" for="inputfile-'+ num +'">选择文件</label></td><td class="col-md-1"><input type="file" id="inputfile-'+ num +'" class="file"></td><td class="col-md-1"><button type="button" class="btn btn-warning fbpx-photo-delete">删除</button></td></tr>')
+  // 修改品项-详情删除选中
+  $('#fbpx-info-delete-all').on('click', function () {
+    $('#fbpx-info-list input[type="checkbox"]:checked').each(function () {
+      $(this).parents('tr').remove();
+    })
   })
 
-  // 品项发布-详情删除一条信息
+  // 品项发布-图片新增一条信息
+  $('#fbpx-photo-add').on('click', function () {
+    num++;
+    $('#fbpx-photo-list tbody').append('<tr><td class="col-md-1"><input type="checkbox" class="checkboxStyle" name="fbpx-photo-list"></td><td class="col-md-6"><input type="text" class="form-control fbpx-inputfile-' + num + '"></td><td class="col-md-1"><label class="btn btn-primary" for="fbpx-inputfile-' + num + '">选择文件</label></td><td class="col-md-1"><input type="file" id="fbpx-inputfile-' + num + '" class="file"></td><td class="col-md-1"><button type="button" class="btn btn-warning fbpx-photo-delete">删除</button></td></tr>')
+  })
+
+  // 品项发布-图片删除一条信息
   $('#fbpx-photo-list tbody').on('click', 'button.fbpx-photo-delete', function () {
     $(this).parents('tr').remove();
   })
 
+  // 修改品项-详情删除选中
+  $('#fbpx-photo-delete-all').on('click', function () {
+    $('#fbpx-photo-list input[type="checkbox"]:checked').each(function () {
+      $(this).parents('tr').remove();
+    })
+  })
 
   // 上传文件获取文件名
   // var filename = $('.fileName');
-    $('#fbpx-photo-list').on('change', '.file', function (e) {
+  $('#fbpx-photo-list').on('change', '.file', function (e) {
     //e.currentTarget.files 是一个数组，如果支持多个文件，则需要遍历
     var name = e.currentTarget.files[0].name;
     var thisIdName = $(this).attr('id')
@@ -46,6 +63,56 @@ $(function () {
   });
 
 
+  // =================================
+  // 修改品项
+  // =================================
+
+  var numA = 1;
+  // 修改品项-详情新增一条信息
+  $('#xgpx-info-add').on('click', function () {
+    $('#xgpx-info-list tbody').append('<tr><td class="col-md-1"><input type="checkbox" class="checkboxStyle" name="xgpx-info-list"></td><td class="col-md-2"><input type="text" class="form-control"></td><td class="col-md-6"><input type="text" class="form-control"></td><td class="col-md-1"><button type="button" class="btn btn-warning xgpx-info-delete">删除</button></td></tr>')
+  })
+
+  // 修改品项-详情删除一条信息
+  $('#xgpx-info-list tbody').on('click', 'button.xgpx-info-delete', function () {
+    $(this).parents('tr').remove();
+  })
+
+
+  // 修改品项-详情删除选中
+  $('#xgpx-info-delete-all').on('click', function () {
+    $('#xgpx-info-list input[type="checkbox"]:checked').each(function () {
+      $(this).parents('tr').remove();
+    })
+  })
+
+  // 修改品项-图片新增一条信息
+  $('#xgpx-photo-add').on('click', function () {
+    numA++;
+    $('#xgpx-photo-list tbody').append('<tr><td class="col-md-1"><input type="checkbox" class="checkboxStyle" name="xgpx-photo-list"></td><td class="col-md-6"><input type="text" class="form-control xgpx-inputfile-' + numA + '"></td><td class="col-md-1"><label class="btn btn-primary" for="xgpx-inputfile-' + numA + '">选择文件</label></td><td class="col-md-1"><input type="file" id="xgpx-inputfile-' + numA + '" class="file"></td><td class="col-md-1"><button type="button" class="btn btn-warning xgpx-photo-delete">删除</button></td></tr>')
+  })
+
+  // 修改品项-图片删除一条信息
+  $('#xgpx-photo-list tbody').on('click', 'button.xgpx-photo-delete', function () {
+    $(this).parents('tr').remove();
+  })
+
+  // 修改品项-详情删除选中
+  $('#xgpx-photo-delete-all').on('click', function () {
+    $('#xgpx-photo-list input[type="checkbox"]:checked').each(function () {
+      $(this).parents('tr').remove();
+    })
+  })
+
+  // 上传文件获取文件名
+  // var filename = $('.fileName');
+  $('#xgpx-photo-list').on('change', '.file', function (e) {
+    //e.currentTarget.files 是一个数组，如果支持多个文件，则需要遍历
+    var name = e.currentTarget.files[0].name;
+    var thisIdName = $(this).attr('id')
+    $('.' + thisIdName).val(name).attr('disabled', 'disabled');
+    console.log(thisIdName)
+  });
 
 
 });
