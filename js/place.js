9cd19e4f54967c68e261f7e331bf3fef13aedf6b
yaocs <yao.cs@qq.com>
2016-09-13 00:26:21 +0800

    update-0913-0:30

--- js/place.js
+++ js/place.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
   // 设置首屏高度
   var height = $(window).height();
   $('.topBox').css('height', height);
@@ -10,11 +10,11 @@ $(document).ready(function() {
   var dailySize = $('#dailySlidePic li').size();
   var dailyPicWidth = $('.dailyPic li').outerWidth();
 
-  $('.daily-slideBtn .next').on('click', function() {
+  $('.daily-slideBtn .next').on('click', function () {
     dailyNext();
   })
 
-  $('.daily-slideBtn .prev').on('click', function() {
+  $('.daily-slideBtn .prev').on('click', function () {
     dailyPrev();
   })
 
@@ -44,17 +44,17 @@ $(document).ready(function() {
     }, 300)
   }
 
-  $('.daily-slideBtn span').each(function() {
-    $(this).hover(function() {
+  $('.daily-slideBtn span').each(function () {
+    $(this).hover(function () {
       $(this).addClass('active')
-    }, function() {
+    }, function () {
       $(this).removeClass('active')
     })
   })
 
   // 设置介绍字段长度 
   var maxTextWidth = 100;
-  $('.introduce').each(function() {
+  $('.introduce').each(function () {
     var curTextWidth = $(this).text().length;
     if (curTextWidth > maxTextWidth) {
       $(this).text($(this).text().substring(0, maxTextWidth));
@@ -65,7 +65,7 @@ $(document).ready(function() {
 
 
   // 滚动事件
-  $(window).scroll(function() {
+  $(window).scroll(function () {
     var windowHeight = $(this).height();
     var scrollHeight = $(this).scrollTop();
 
@@ -99,19 +99,19 @@ $(document).ready(function() {
       }
     }
 
-    $('.container .flyLeftAnimate').each(function() {
+    $('.container .flyLeftAnimate').each(function () {
       flyLeftAnimate(this, 30);
     })
 
-    $('.container .flyRightAnimate').each(function() {
+    $('.container .flyRightAnimate').each(function () {
       flyRightAnimate(this, 30);
     })
 
-    $('.about-container .flyLeftAnimate').each(function() {
+    $('.about-container .flyLeftAnimate').each(function () {
       flyLeftAnimate(this, 20);
     })
 
-    $('.about-container .flyRightAnimate').each(function() {
+    $('.about-container .flyRightAnimate').each(function () {
       flyRightAnimate(this, 20);
     })
 
@@ -123,7 +123,7 @@ $(document).ready(function() {
 
 
   // 回到顶部
-  $('.stick .backtop').on('click', function() {
+  $('.stick .backtop').on('click', function () {
     $('body').animate({
       scrollTop: 0
     }, 300);
@@ -135,9 +135,9 @@ $(document).ready(function() {
   var erweimaH = $('.erweima').outerHeight();
   var erweimaH1 = erweimaH / 2 - stickH / 2;
   $('.erweima').css('bottom', 80 - erweimaH1 + 'px');
-  $('.stick .wechat').hover(function() {
+  $('.stick .wechat').hover(function () {
     $('.erweima').show();
-  }, function() {
+  }, function () {
     $('.erweima').hide();
   })
 
@@ -146,32 +146,20 @@ $(document).ready(function() {
   // =================
 
   // 详情弹出
-  $('.detailBtn').each(function() {
-    $(this).on('click', function() {
-      $('.detailCover').stop().animate({
-        left: 0
-      }, 500).show();
-      $('body').css('overflow-y', 'hidden');
-    })
-  })
 
-  $('.detailBtnTouch').each(function() {
-    $(this).on('click', function() {
-      $('.detailCover').stop().animate({
-        left: 0
-      }, 500).show();
-      $('body').css('overflow-y', 'hidden');
-    })
-  })
 
   // 关闭按钮
-  $('#closeBtn').on('click', function() {
+  $('#closeBtn').on('click', function () {
     $(this).parents('.detailCover').stop().animate({
       left: '100%'
     }, 500).hide(500);
     $('body').css('overflow-y', 'auto');
   })
 
+
+  // 设置图片宽度
+  
+  
   // 详情轮播
   var dtIndex = 0;
   $('.picSlideSmall li').eq(0).addClass('active').siblings().removeClass('active');
@@ -181,7 +169,7 @@ $(document).ready(function() {
   $('.totalNum').text(dtImgSize - 1);
   $('.curNum').text('1');
 
-  $('#picSlideBtnNext').on('click', function() {
+  $('#picSlideBtnNext').on('click', function () {
     dtIndex += 1;
     if (dtIndex == dtImgSize) {
       $('.picSlideBig').css('left', 0);
@@ -189,22 +177,22 @@ $(document).ready(function() {
     }
     $('.curNum').text(dtIndex + 1);
     $('.picSlideBig').stop().animate({
-      'left': -dtIndex * 1100 + 'px'
+      'left': -dtIndex * widthaaa + 'px'
     }, 300);
     if (dtIndex == dtImgSize - 1) {
       $('.curNum').text('1');
     }
   })
 
-  $('#picSlideBtnPrev').on('click', function() {
+  $('#picSlideBtnPrev').on('click', function () {
     dtIndex -= 1;
     if (dtIndex == -1) {
-      $('.picSlideBig').css('left', -(dtImgSize - 1) * 1100);
+      $('.picSlideBig').css('left', -(dtImgSize - 1) * widthaaa);
       dtIndex = dtImgSize - 2;
     }
     $('.curNum').text(dtIndex + 1);
     $('.picSlideBig').stop().animate({
-      'left': -dtIndex * 1100 + 'px'
+      'left': -dtIndex * widthaaa + 'px'
     }, 300);
   })
 
@@ -215,7 +203,7 @@ $(document).ready(function() {
   var height = $(window).height();
   $('.responsiveNav-list').css('height', height);
   var responsiveNav = document.getElementById('responsiveNav');
-  responsiveNav.ontouchstart = function() {
+  responsiveNav.ontouchstart = function () {
     // $('.responsiveNav').on('click', function () {
     if ($('.responsiveNav-list').css('left') != '0px') {
       $('.responsiveNav-list').animate({
@@ -239,14 +227,16 @@ $(document).ready(function() {
   // 页面数据获取部分
   // =================
   // var curID = [];
+  var widthaaa;
+  var heightaaa;
   $.ajax({
     type: 'GET',
     url: '../js/data/allTitle.json',
     data: 'id=' + 1,
     dataType: 'JSON',
-    success: function(data) {
+    success: function (data) {
       var title = data.data.class;
-      $.each(title, function(i, cur) {
+      $.each(title, function (i, cur) {
         $('#placeList').append('<section class="typeList clearfix"><div class="typeList-title flyLeftAnimate" id="' + cur.id + '"><h2>' + cur.name + '</h2><span><i></i><i></i><i></i></span></div><div class="colList clearfix flyRightAnimate"></div></section>')
       })
     }
@@ -256,14 +246,37 @@ $(document).ready(function() {
     type: "GET",
     url: "../js/data/place-list.json",
     dataType: "JSON",
-    success: function(data) {
+    success: function (data) {
       var item = data.data.item;
-      $.each(item, function(i, cur) {
+      $.each(item, function (i, cur) {
         var curId = cur.styleId;
         $('#' + curId).siblings('.colList').append('<div class="product left"><div class="productPic"><img src="' + cur.cover + '" alt="123"></div><div class="productPic-info detailBtnTouch">' + cur.title + '</div><div class="product-content-bg"><div class="product-content"><h3>' + cur.title + '</h3><p class="introduce">' + cur.brief + '</p><span class="productStrip"></span></div></div><div class="detailBtn productBtn">VIEW</div></div>')
-          // 设置分类标题高度
+        // 设置分类标题高度
         $('.typeList-title').height($('.colList .product').height());
       })
+      $('.detailBtn').each(function () {
+        $(this).on('click', function () {
+          $('.detailCover').stop().animate({
+            left: 0
+          }, 500).show();
+          $('body').css('overflow-y', 'hidden');
+          widthaaa = $('.picSlideBox').width();
+          heightaaa = $('.picSlideBig li').height();
+          console.log(heightaaa)
+          $('.picSlideBig li').css('width', widthaaa + 'px');
+          // $('.picSlideBox').css('height', heightaaa + 'px');
+        })
+      })
+
+      $('.detailBtnTouch').each(function () {
+        $(this).on('click', function () {
+          $('.detailCover').stop().animate({
+            left: 0
+          }, 500).show();
+          $('body').css('overflow-y', 'hidden');
+        })
+      })
+      
     }
   });
 
